895d93051f071babba21bf5265b44d0fe7ea8de3
KrazyDev <67275060+KartikeSingh@users.noreply.github.com>
2022-03-17 19:30:41 +0530

    Hell yeah

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const { Client } = require("discord.js");
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
+const { config } = require('dotenv');
+const messages = require('./db.json');
+
+config();
 
 const bot = new Client({
   intents: 32767,
-  allowedMentions: { parse: ["users", "roles"], repliedUser: true },
 });
 
 bot.on("ready", () => {
   console.log(`${bot.user.tag} is online!`);
 });
 
-
 bot.on("messageCreate", (message) => {
   if (message.author.id === bot.user.id) return;
-  let data = fs.readFileSync(path.resolve(__dirname, "db.json"));
-  const shabda = JSON.parse(data);
 
-  for (a in shabda) {
-    if (message.content.toLowerCase().includes(a)) {
-      return message.reply(`${a}  ❌\n${shabda[a].alt}  ✅`);
-    }
-  }
+  const match = messages.filter(v => message.toLowerCase().includes(v.toLowerCase()));
+
+  if (match.length === 0) return;
+
+  message.reply(`❌ ${match.map(v => v.wrong).join(", ")}\n✅ ${match.map(v => v.right).join(", ")}`)
 });
 
 bot.login(process.env.TOKEN);
