4c70e17e324959a4f8c32cd6513a2160e56fe80d
DoctorCo <78472859+DoctorCo@users.noreply.github.com>
2022-04-01 15:20:24 +0530

    Delete src directory

--- src/events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = (bot) => {
-  console.log(`${bot.user.tag} is online!`);
-
-  bot.user.setPresence({
-    activities: [{ name: "Bhagvad Geeta", type: "LISTENING" }],
-  });
-};
--- src/index.js
+++
@@ -1,4 +0,0 @@
-const { config } = require("dotenv");
-config();
-const bot = new (require("../constructors/Govindam"))();
-bot.start(process.env.token)
