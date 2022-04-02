1615132c3717bedb266c70d8cff8dda033a385db
DoctorCo <78472859+DoctorCo@users.noreply.github.com>
2022-04-02 21:28:38 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const Levels = require("discord-xp");
-Levels.setURL(process.env.mongodb);
-const bot = new (require("../constructors/Govindam"))();
-bot.start(process.env.token);
