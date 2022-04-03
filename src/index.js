7ae9b3630c051f287708e0306ded0eeddb830fbe
DoctorCo <78472859+DoctorCo@users.noreply.github.com>
2022-04-03 13:36:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const Levels = require("discord-xp");
+require("dotenv").config();
 Levels.setURL(process.env.mongodb);
 const bot = new (require("../constructors/Govindam"))();
 bot.start(process.env.token);
